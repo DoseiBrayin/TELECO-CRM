f51d53d6a6a2e1ffed58dbfddaa2cc369d18c890
DoseiBrayin <ardilabrayan68@gmail.com>
2022-09-23 23:30:07 -0500

    Fix & Add
    Fixing reitarative error with nodemon services and adding the express config

--- src/views/index.js
+++ src/views/index.js
@@ -1,20 +1,41 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path')
 
 //Initializations
 const app = express();
 
 //Settings
 app.set('port',process.env.PORT || 4000);
+app.set('views', path.join(__dirname,'views'))
+app.engine('.hbs', handlebars.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'Layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('../lib/handlebars.js')
+}));
+app.set('view engine', '.hbs')
 
 //Middlewarea
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Global Variables
+app.use((req,res,next)=>{
+    next();
+});
 
 //Routes
+app.use(require('../routes/'));
+app.use(require('../routes/auth'));
+app.use('/links',require('../routes/links'));
+
 
 //Public files
+app.use(express.static(path.join(__dirname,'public')));
 
 //Starting the server
 app.listen(app.get('port'), () => {
