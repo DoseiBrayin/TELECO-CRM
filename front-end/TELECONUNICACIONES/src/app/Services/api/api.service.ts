32a481c7cdc56350a72bc976b308c061d220244c
DoseiBrayin <ardilabrayan68@gmail.com>
2022-10-12 02:36:12 -0500

    (Feat): adding and sending requests from the front-end

--- front-end/TELECONUNICACIONES/src/app/Services/api/api.service.ts
+++ front-end/TELECONUNICACIONES/src/app/Services/api/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {clientI} from '../../model/client.interface'
-import {ResponseI} from '../../model/response.interface'
-import {HttpClient,HttpHeaders} from '@angular/common/http'
-import {from, Observable} from 'rxjs'
+import { clientI } from '../../model/client.interface'
+import { PetitionI } from '../../model/petition.interface'
+import { ResponseI } from '../../model/response.interface'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { from, Observable } from 'rxjs'
 
 
 @Injectable({
@@ -10,17 +11,26 @@ import {from, Observable} from 'rxjs'
 })
 export class ApiService {
 
-  url:string = 'http://localhost:4000/';
+  url: string = 'http://localhost:4000/';
 
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) {
   }
 
-  loginBy(form:clientI):Observable<ResponseI>{
+  loginBy(form: clientI): Observable<ResponseI> {
     let dir = this.url + "autenticar"
-    return this.http.post<ResponseI>(dir,{
+    return this.http.post<ResponseI>(dir, {
       "username": form.username,
       "password": form.password
-  })
+    })
+  }
+
+  petitionBy(form: PetitionI): Observable<ResponseI> {
+    let dir = this.url + "links/client/petition"
+    return this.http.post<ResponseI>(dir, {
+      "client_id": form.client_id,
+      "email": form.email,
+      "des": form.des,
+    })
   }
 
 
--- front-end/TELECONUNICACIONES/src/app/request-usuario/request-usuario.component.ts
+++ front-end/TELECONUNICACIONES/src/app/request-usuario/request-usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validator, Validators} from '@angular/forms';
+import { PetitionI } from '../model/petition.interface';
+import {ApiService} from '../../app/Services/api/api.service';
+import { ResponseI } from '../model/response.interface';
+
 
 @Component({
   selector: 'app-request-usuario',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestUsuarioComponent implements OnInit {
 
-  constructor() { }
+  petitionForm = new FormGroup({
+    client_id:new FormControl('',Validators.required),
+    email:new FormControl('',Validators.required),
+    des:new FormControl('',Validators.required)
+  })
+
+  constructor(private api:ApiService) { }
 
   ngOnInit(): void {
   }
 
+  onPetition(form:PetitionI){
+    this.api.petitionBy(form).subscribe(data=>{
+      let DataPetition = data;
+      console.log(DataPetition)
+    })
+  }
 }
