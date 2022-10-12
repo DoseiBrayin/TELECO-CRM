e02d9cbcb1e484f750561ebfe962c9f25d0608fd
DoseiBrayin <ardilabrayan68@gmail.com>
2022-10-12 12:37:08 -0500

    (Fix): Fixing router request-user

--- front-end/TELECONUNICACIONES/src/app/app.module.ts
+++ front-end/TELECONUNICACIONES/src/app/app.module.ts
@@ -15,6 +15,8 @@ import {MatDialogModule} from '@angular/material/dialog';
 
 import { IndexAdminComponent } from './index-admin/index-admin.component';
 
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
